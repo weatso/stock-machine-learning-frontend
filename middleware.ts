7d193f901b35752463be8a61074fe90acd33b871
Natanael2412 <natanaelalexandergani@gmail.com>
2026-04-09 15:50:52 +0700

    Middleware error

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value
